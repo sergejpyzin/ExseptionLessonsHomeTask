8d185e0b9e9a7d16b45925d5cade67885bef54d8
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-25 10:56:13 +0300

    changed TaskThird

--- src/main/java/LessonTwo/TaskThird.java
+++ src/main/java/LessonTwo/TaskThird.java
@@ -1,4 +1,24 @@
 package LessonTwo;
 
 public class TaskThird {
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.err.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.err.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.err.println("Что-то пошло не так...");
+        }
+    }
+    public static void printSum(Integer a, Integer b){
+        System.out.println(a + b);
+    }
+
 }
