fb2d80e7b179c96191187ad44f96e8e6a424fe97
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-07-29 16:37:57 +0300

    changed UniversityApp and finished work

--- src/main/java/LessonThird/Main.java
+++ src/main/java/LessonThird/Main.java
@@ -10,86 +10,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        /*
-        String[] date = requestInformation();
-        String gender = checkInputGender(date);
-        System.out.println(gender);
-        int phoneNumber = parseArrayElementInteger(date);
-
-        Human me = new Human(date[0], date[1], date[2], date[3], phoneNumber, gender);
-        System.out.println(me);
-        FileWorker.write(me);
-        */
-        work();
+//        UniversityApp.work();
+        UniversityApp.anotherWork();
 
 
     }
 
-    static void work() {
-        Scanner sc = new Scanner(System.in);
-        String exit;
-        do {
-            System.out.println("""
-                    Введите информацию о пользователе в следующем формате:
-                    Фамилия Имя Отчество дата_рождения номер_телефона пол  - через пробел, без знаков препинания;
-                    номер_телефона - целое беззнаковое число;
-                    пол:
-                    m - мужской
-                    f - женский
-                    """);
-            String[] userDate = requestInformation();
-            int phoneNumber = parseArrayElement(userDate);
-            String gender;
-            if (checkInputGender(userDate)) {
-                gender = userDate[5];
-            } else {
-                throw new RuntimeException("Введено неверное определение пола");
-            }
-            Human human = new Human(userDate[0], userDate[1], userDate[2], userDate[3], phoneNumber, gender);
-            FileWorker.write(human);
-            System.out.println("Вы хотите продолжить? q - выход из программы, y - продолжить");
-            exit = sc.nextLine();
-        } while (!exit.equalsIgnoreCase("q"));
-        System.exit(0);
-
-    }
-
-    static String[] requestInformation() {
-        Scanner scanner = new Scanner(System.in);
-        String[] userAnswer = scanner.nextLine().replace(",", ".").split("\\s+");
-        if (userAnswer.length < 6) {
-            try {
-                throw new ArrayIndexOutOfBoundsException();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
-            }
-        }
-        if (userAnswer.length > 6) {
-            try {
-                throw new ArrayIndexOutOfBoundsException();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
-            }
-        }
-        return userAnswer;
-    }
-
-    static int parseArrayElement(String[] someArray) {
-        int arrayElement = 0;
-        try {
-            arrayElement = Integer.parseInt(someArray[4]);
-        } catch (NumberFormatException e) {
-            System.err.println("Введено не число!");
-        }
-        return arrayElement;
-    }
-
-    static boolean checkInputGender(String[] someArray) {
-        String gender = "";
-        if (someArray[5].equals("f") || someArray[5].equals("m")) {
-            return true;
-        }
-        return false;
-    }
 
 }
--- src/main/java/LessonThird/UniversityApp.java
+++ src/main/java/LessonThird/UniversityApp.java
@@ -1,4 +1,104 @@
 package LessonThird;
 
+import java.util.Scanner;
+
 public class UniversityApp {
+
+    static void anotherWork() {
+        System.out.println("""
+                Введите информацию о пользователе в следующем формате:
+                Фамилия Имя Отчество дата_рождения номер_телефона пол  - через пробел, без знаков препинания;
+                номер_телефона - целое беззнаковое число;
+                пол:
+                m - мужской
+                f - женский
+                """);
+        String[] userDate = requestInformation();
+        int phoneNumber = parseArrayElement(userDate);
+        String gender = "";
+        if (checkInputGender(userDate)) {
+            gender = userDate[5];
+        } else {
+            try {
+                throw new RuntimeException("Введено неверное определение пола");
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
+        }
+        Human human = new Human(userDate[0], userDate[1], userDate[2], userDate[3], phoneNumber, gender);
+        FileWorker.write(human);
+    }
+
+    static void work() {
+        Scanner sc = new Scanner(System.in);
+        String exit;
+        do {
+            System.out.println("""
+                    Введите информацию о пользователе в следующем формате:
+                    Фамилия Имя Отчество дата_рождения номер_телефона пол  - через пробел, без знаков препинания;
+                    номер_телефона - целое беззнаковое число;
+                    пол:
+                    m - мужской
+                    f - женский
+                    """);
+            String[] userDate = requestInformation();
+            int phoneNumber = parseArrayElement(userDate);
+            String gender = "";
+            if (checkInputGender(userDate)) {
+                gender = userDate[5];
+            } else {
+                try {
+                    throw new RuntimeException("Введено неверное определение пола");
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+            Human human = new Human(userDate[0], userDate[1], userDate[2], userDate[3], phoneNumber, gender);
+            FileWorker.write(human);
+            System.out.println("Вы хотите продолжить? q - выход из программы, y - продолжить");
+            exit = sc.nextLine();
+        } while (!exit.equalsIgnoreCase("q"));
+        System.exit(0);
+
+    }
+
+    private static String[] requestInformation() {
+        Scanner scanner = new Scanner(System.in);
+        String[] userAnswer = scanner.nextLine().replace(",", ".").split("\\s+");
+        if (userAnswer.length < 6) {
+            try {
+                throw new ArrayIndexOutOfBoundsException("Введенная информация меньше необходимой!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+        if (userAnswer.length > 6) {
+            try {
+                throw new ArrayIndexOutOfBoundsException("Введенная информация больше необходимой!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+        return userAnswer;
+    }
+
+    private static int parseArrayElement(String[] someArray) {
+        int arrayElement = 0;
+        try {
+            arrayElement = Integer.parseInt(someArray[4]);
+        } catch (NumberFormatException e) {
+            System.out.println("Введено не число!");
+            e.printStackTrace();
+        }
+        return arrayElement;
+    }
+
+    private static boolean checkInputGender(String[] someArray) {
+        String gender = "";
+        if (someArray[5].equals("f") || someArray[5].equals("m")) {
+            return true;
+        }
+        return false;
+    }
+
 }
